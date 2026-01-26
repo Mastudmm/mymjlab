63de8571cdc168b661775c02bc1ddc8f527c2b19
Mastudmm <1808003650@qq.com>
2026-01-26 23:51:12 +0800

    复现20号楼梯抬脚,2clearance加上

--- src/mjlab/tasks/velocity/config/go1/env_cfgs.py
+++ src/mjlab/tasks/velocity/config/go1/env_cfgs.py
@@ -217,12 +217,12 @@ def unitree_go1_rough_env_cfg(play: bool = False) -> ManagerBasedRlEnvCfg:
     thigh_ground_cfg.name,
   ]
   cfg.rewards["stumble"].weight = -0.5 # Increased penalty to force leg lifting
-  cfg.rewards["foot_clearance"].weight = 0.0
+  cfg.rewards["foot_clearance"].weight = -0.05
   cfg.rewards["foot_swing_height"].weight = -0.175 # Penalty for deviation from target height (MUST BE NEGATIVE)
   cfg.rewards["stumble"].params={
         "sensor_names": ["feet_ground_contact","calf_ground_contact"],
       }
-  cfg.rewards["foot_slip"].weight = -0.85
+  cfg.rewards["foot_slip"].weight = -0.5
 
   cfg.terminations["illegal_contact"] = TerminationTermCfg(
     func=mdp.illegal_contact,
