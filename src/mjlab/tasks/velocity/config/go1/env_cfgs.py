a21fd32e57c3f5c53dbdab79fd544e0fa558eeeb
Mastudmm <1808003650@qq.com>
2026-01-31 11:29:38 +0800

    调参pose和clearance

--- src/mjlab/tasks/velocity/config/go1/env_cfgs.py
+++ src/mjlab/tasks/velocity/config/go1/env_cfgs.py
@@ -178,8 +178,8 @@ def unitree_go1_rough_env_cfg(play: bool = False) -> ManagerBasedRlEnvCfg:
   }
   cfg.rewards["pose"].params["std_walking"] = {
     r".*(FR|FL|RR|RL)_hip_joint.*": 0.325,
-    r".*(FR|FL|RR|RL)_thigh_joint.*": 0.375,
-    r".*(FR|FL|RR|RL)_calf_joint.*": 0.55,
+    r".*(FR|FL|RR|RL)_thigh_joint.*": 0.4,
+    r".*(FR|FL|RR|RL)_calf_joint.*": 0.65,
   }
   cfg.rewards["pose"].params["std_running"] = {
     r".*(FR|FL|RR|RL)_hip_joint.*": 0.375,
@@ -211,17 +211,17 @@ def unitree_go1_rough_env_cfg(play: bool = False) -> ManagerBasedRlEnvCfg:
   cfg.rewards["air_time"].weight = 0.35
   # Override base placeholder reward: bind sensor + weight.
   cfg.rewards["calf_collision"].params["sensor_name"] = calf_ground_cfg.name
-  cfg.rewards["calf_collision"].params["threshold"] = 5.0  # Allow grazing contacts < 15N
+  cfg.rewards["calf_collision"].params["threshold"] = 3.0  # Allow grazing contacts < 15N
   cfg.rewards["calf_collision"].weight = -0.75  # tweak within [-1.0, -3.0]
   cfg.rewards["thigh_collision"].params["sensor_name"] = thigh_ground_cfg.name
-  cfg.rewards["thigh_collision"].params["threshold"] = 5.0
+  cfg.rewards["thigh_collision"].params["threshold"] = 3.0
   cfg.rewards["thigh_collision"].weight = -0.25  # tweak within [-1.0, -3.0]
   cfg.rewards["stumble"].params["sensor_names"] = [
     calf_ground_cfg.name,   
     thigh_ground_cfg.name,
   ]
   cfg.rewards["stumble"].weight = -0.5 # Increased penalty to force leg lifting
-  cfg.rewards["foot_clearance"].weight = -0.01
+  cfg.rewards["foot_clearance"].weight = -0.1
   cfg.rewards["foot_swing_height"].weight = -1.25 # Penalty for deviation from target height (MUST BE NEGATIVE)
   cfg.rewards["stumble"].params={
         "sensor_names": ["feet_ground_contact"],
